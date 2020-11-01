2ebb32bfed7eb7f710ad19dce64b637c469ef652
sania7594 <47710445+sania7594@users.noreply.github.com>
2020-11-01 23:05:13 +0300

    Project creation

---
+++ src/test/java/Netcracker/AppTest.java
@@ -0,0 +1,20 @@
+package Netcracker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
